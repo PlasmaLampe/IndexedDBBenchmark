11fc2bf45f006de92b0235bfd881408fe5dec513
Joerg Amelunxen <jamelunx@mail.uni-paderborn.de>
2017-06-02 21:27:09 +0200

    Added simple application logic

--- benchmark.js
+++ benchmark.js
@@ -1,54 +1,134 @@
 'use strict';
 
-function createJSONStructure(propertyAmount)
-{
-    let o = {};
+
+// ------------------------------------------------------
+// Helper and utility functions
+// ------------------------------------------------------
+function createJSONStructureArr(propertyAmount, amountItems, idOffset) {
+    let createdItems = [];
     
-    for (let i = 0; i < propertyAmount; i++) {
-        o['prop' + i] = 'someText';
+    if(typeof idOffset === 'undefined'){
+    	idOffset = 0;
     }
+    
+    for(let itemNr = 0 + idOffset; itemNr < amountItems + idOffset; itemNr++){
+    	let o = {};
+
+	    for (let i = 0; i < propertyAmount; i++) {
+	        o['prop' + i] = 'someText';
+	    }
+
+	   	// set id of item
+	    o['id'] = itemNr;
+
+	    createdItems.push(o);
+    }
+
+    return createdItems;
 }
 
-createJSONStructure(25);
+function storeInThisTransaction(tx, dataItem) {
+    //var tx = db.transaction("MyObjectStore", "readwrite");
+    let store = tx.objectStore("Benchmark");
+    //let index = store.index("NameIndex");
 
-// This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
-var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+    store.put(dataItem);
+}
+
+function storeInNewTransaction() {
+	let tx = db.transaction("Benchmark", "readwrite");
+    let store = tx.objectStore("Benchmark");
+    //let index = store.index("NameIndex");
+
+    throw 'niy'
+}
+
+function loadItemViaId_promise(id) {
+	let tx = db.transaction("Benchmark", "readwrite");
+    let store = tx.objectStore("Benchmark");
+
+	const getItem = store.get(id);
+
+	return new Promise((resolve, reject) => {
+		getItem.onsuccess = () => {
+			resolve(getItem.result);
+		}
+	});
+}
+
+function clearDB_promise() {
+	let tx = db.transaction("Benchmark", "readwrite");
+	let objectStore = tx.objectStore("Benchmark");
+	let objectStoreRequest = objectStore.clear();
+
+	return new Promise((resolve, reject) => {
+		objectStoreRequest.onsuccess = function(event) {
+			console.log("db cleared");
+			resolve();
+	  	};
+	}); 
+}
+
+// ------------------------------------------------------
+// App bootstrap
+// ------------------------------------------------------
 
 // Open (or create) the database
-var open = indexedDB.open("MyDatabase", 1);
+var open = indexedDB.open("MyDatabase", 2);
+var db = null;
 
 // Create the schema
 open.onupgradeneeded = function() {
     var db = open.result;
-    var store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
-    var index = store.createIndex("NameIndex", ["name.last", "name.first"]);
+    var store = db.createObjectStore("Benchmark", {keyPath: "id"});
+    var index = store.createIndex("prop1", "prop1");
 };
 
 open.onsuccess = function() {
-    // Start a new transaction
-    var db = open.result;
-    var tx = db.transaction("MyObjectStore", "readwrite");
-    var store = tx.objectStore("MyObjectStore");
-    var index = store.index("NameIndex");
+    // open database
+    db = open.result;
 
-    // Add some data
-    store.put({id: 12345, name: {first: "John", last: "Doe"}, age: 42});
-    store.put({id: 67890, name: {first: "Bob", last: "Smith"}, age: 35});
-    
-    // Query the data
-    var getJohn = store.get(12345);
-    var getBob = index.get(["Smith", "Bob"]);
-
-    getJohn.onsuccess = function() {
-        console.log(getJohn.result.name.first);  // => "John"
-    };
-
-    getBob.onsuccess = function() {
-        console.log(getBob.result.name.first);   // => "Bob"
-    };
+    clearDB_promise().then(() => {
+    	benchmark(db);
+    });
 
     // Close the db when the transaction is done
-    tx.oncomplete = function() {
-        db.close();
-    };
+    //tx.oncomplete = function() {
+     //   db.close();
+    //};
+}
+
+// ------------------------------------------------------
+// Main Benchmark Application
+// ------------------------------------------------------
+function benchmark(db) {
+
+	// TC1: add 100 items
+	let tx = db.transaction("Benchmark", "readwrite");
+	const items = createJSONStructureArr(25,100,0);
+
+	for(const item of items){
+		storeInThisTransaction(tx, item);
+	}
+
+	let returnPromise = new Promise((resolve, reject) => {
+		tx.oncomplete = function() {
+        	resolve();
+    	};
+	});
+
+	returnPromise.then(() => {
+		// TC2: load items
+		loadItemViaId_promise(1).then((item) => {
+			console.log(item);
+		});
+	});
+
+	// FIXME: Create chained test cases and measure time
+
+	// Include test case without primary key!!!! I want the timing there
+
+
+
+
 }
